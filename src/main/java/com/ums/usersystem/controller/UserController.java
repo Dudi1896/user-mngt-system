202fdec07405388c80288f603fe13f74567db618
Dudi1896 <denzelu181@gmail.com>
2023-06-19 16:13:50 -0400

    Controller

--- src/main/java/com/ums/usersystem/controller/UserController.java
+++ src/main/java/com/ums/usersystem/controller/UserController.java
@@ -2,9 +2,12 @@ package com.ums.usersystem.controller;
 
 import com.ums.usersystem.model.User;
 import com.ums.usersystem.service.UserService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -16,8 +19,35 @@ public class UserController {
         this.userService = userService;
     }
 
-    @PostMapping("/user")
-    public User saveUser(User user) {
+    @PostMapping("/users")
+    public User saveUser(@RequestBody User user) {
         return userService.saveUser(user);
+    }
+
+    @GetMapping("/users")
+    public List<User> getAllUsers(){
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/users/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable("id") Long id){
+        User user = null;
+        user = userService.getUserById(id);
+        return ResponseEntity.ok(user);
+    }
+
+    @DeleteMapping("/users/{id}")
+    public ResponseEntity<Map<String, Boolean>> deleteEmployee(@PathVariable("id") Long id){
+        boolean deleted = false;
+        deleted = userService.deleteUser(id);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", deleted);
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping("/users/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable("id") Long id, @RequestBody User user) {
+        user = userService.updateUser(id, user);
+        return ResponseEntity.ok(user);
     }
 }
