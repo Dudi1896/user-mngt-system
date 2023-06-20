4524e23aa8d851b166d4a894f00c2cab6804bea8
Dudi1896 <denzelu181@gmail.com>
2023-06-19 20:09:03 -0400

    front-end

---
+++ next-front-end/pages/index.js
@@ -0,0 +1,14 @@
+
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>User Management App</title>
+      </Head>
+
+      <main>User Management App</main>
+    </div>
+  )
+}
